acaaeb2cd147b425381a1fc5d2d363f717de9cd1
Victor Antonovich <v.antonovich@gmail.com>
2014-05-12 23:12:10 +0400

    Some dirty fix for correct flatpages localization for 'en' locale.

--- marcus/context_processors.py
+++ marcus/context_processors.py
@@ -1,10 +1,14 @@
 from django.contrib.sites.models import Site
 from django.conf import settings
-
+from django.utils import translation
 
 def marcus_context(request):
+    language = 'en' if request.path.endswith('/en/') else None
+    if language:
+        translation.activate(language)
     return {
         'site': Site.objects.get_current(),
+        'language': language,
         'MARCUS_TITLE': settings.MARCUS_TITLE,
         'MARCUS_SUBTITLE': settings.MARCUS_SUBTITLE,
         'MARCUS_GA_ACCOUNT_ID' : settings.MARCUS_GA_ACCOUNT_ID,
