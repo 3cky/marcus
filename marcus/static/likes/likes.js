c8b113ac2cf55176f9d032676bb14b76b764d714
Victor Antonovich <v.antonovich@gmail.com>
2019-03-29 13:22:01 +0400

    Update jQuery to 3.3.1

--- marcus/static/likes/likes.js
+++ marcus/static/likes/likes.js
@@ -1,14 +1,16 @@
 $(document).ready(function() {
-	$(".likes-link").on('click', function(event) {
-		event.preventDefault();
-		var el = $(this);
-		var replace_target = el.parents('.likes:first');
-		$(".likes-heart")
-		.on('animationend', function() {
-			$.get(el.attr('href'), {}, function(data) {
-				replace_target.html(data);
-			});
+	$.each($(".likes-link"), function() {
+		$(this).on('click', function(event) {
+			event.preventDefault();
+			var el = $(this);
+			var replace_target = el.parents('.likes:first');
+			$(".likes-heart")
+			.on('animationend', function() {
+				$.get(el.attr('href'), {}, function(data) {
+					replace_target.html(data);
+				});
+			})
+			.toggleClass('likes-heart-clicked');
 		})
-		.toggleClass('likes-heart-clicked');
 	});
 });
